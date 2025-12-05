4a69cb068034aeda69b101c2d0cdcec4ef77db96
umair <umairkhn76@gmail.com>
2025-12-05 21:12:35 +0530

    Change email

--- root/root/settings.py
+++ root/root/settings.py
@@ -178,9 +178,9 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "hifzan56@gmail.com"
+EMAIL_HOST_USER = "umairkhn76@gmail.com"
 EMAIL_HOST_PASSWORD = "zfso nnwd cbqs hnil"
-DEFAULT_FROM_EMAIL = "root <hifzan56@gmail.com>"
+DEFAULT_FROM_EMAIL = "root <umairkhn76@gmail.com>"
 
 DEFAULT_DOMAIN = "127.0.0.1:8000"
 
