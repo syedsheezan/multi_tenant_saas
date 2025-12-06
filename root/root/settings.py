90e809fdcbba3c68452a0944689a8e2e952ab46a
umair <umairkhn76@gmail.com>
2025-12-06 12:57:58 +0530

    Add middleware file

--- root/root/settings.py
+++ root/root/settings.py
@@ -64,6 +64,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'apps.tenants.middleware.TenantMiddleware',
 ]
 ROOT_URLCONF = 'root.urls'
 
